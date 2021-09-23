fixing data parallel loss calculation

graph_kernel.py
@@ -333,7 +333,8 @@ def train(model, train_loader, optimizer, loss_fn, device):
         # loss = torch.norm(out.view(-1) - batch.y.view(-1), 1)
         # loss.backward()
 
-        l2 = loss_fn(out.view(args.batch_size, -1), batch.y.view(args.batch_size, -1))
+        concat_y = torch.cat([data.y for data in batch])
+        l2 = loss_fn(out.view(args.batch_size, -1), concat_y.view(args.batch_size, -1))
         l2.backward()
 
         optimizer.step()
@@ -351,8 +352,9 @@ def validate(model, valid_loader, loss_fn, device):
         for batch in valid_loader:
             # data = batch.to(device, non_blocking=args.non_blocking)
             out = model(data)
+            concat_y = torch.cat([data.y for data in batch])
             avg_loss += loss_fn(
-                out.view(args.batch_size, -1), batch.y.view(args.batch_size, -1)
+                out.view(args.batch_size, -1),  concat_y.view(args.batch_size, -1)
             ).item()
     avg_loss /= len(valid_loader)
     return avg_loss