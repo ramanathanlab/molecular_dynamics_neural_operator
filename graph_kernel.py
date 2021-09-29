ac3d57e363b427181f2cc844dd7dc788b5bc4b20
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2021-09-29 13:33:45 -0700

    Update graph_kernel.py

--- graph_kernel.py
+++ graph_kernel.py
@@ -386,7 +386,6 @@ def construct_pairdata(x_position, x_aminoacid, previous_x_positions, threshold:
 
 
 def recursive_propagation(model, dataset, device, num_steps: int, starting_points: list, threshold: float = 8.0):
-    pdb.set_trace()
     forecasts = []
     model.eval()
     with torch.no_grad():
