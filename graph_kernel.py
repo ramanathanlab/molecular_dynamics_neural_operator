18e589f7a679d675a26fa417c9570c52307271eb
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2021-10-01 10:08:45 -0700

    Update graph_kernel.py

--- graph_kernel.py
+++ graph_kernel.py
@@ -273,8 +273,8 @@ class KernelNN(torch.nn.Module):
 
     def forward(self, data: PairData, return_latent: bool = False, single_example: bool = False) -> [torch.Tensor, Optional[torch.tensor]]:
         edge_index, edge_attr = data.edge_index, data.edge_attr
-        x = data.x_position.reshape(-1, args.window_size, args.num_residues, 3)
-        x, hidden = self.lstm(x)
+        # x = data.x_position.reshape(-1, args.window_size, args.num_residues, 3)
+        x, hidden = self.lstm(data.x_position)
         x = self.lstm_fc(x)
         # Use an embedding layer to map the onehot aminoacid vector to
         # a dense vector and then concatenate the result with the positions
