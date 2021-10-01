5f011fb3b1b0b0acec228fd932c2d92ca345e5a9
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2021-10-01 11:27:52 -0700

    remove pdb call

--- graph_kernel.py
+++ graph_kernel.py
@@ -273,7 +273,6 @@ class KernelNN(torch.nn.Module):
 
     def forward(self, data: PairData, return_latent: bool = False, single_example: bool = False) -> [torch.Tensor, Optional[torch.tensor]]:
         edge_index, edge_attr = data.edge_index, data.edge_attr
-        pdb.set_trace()
         x = data.x_position.reshape(-1, args.window_size, args.num_residues, 3)
         x, hidden = self.lstm(data.x_position)
         # take the last time slice, we don't want all of them
