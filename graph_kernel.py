42a0d2f0679a8692ff50fb9efd8e0d0cd3b76cb7
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2021-10-21 17:37:35 -0700

    Update graph_kernel.py

--- graph_kernel.py
+++ graph_kernel.py
@@ -262,7 +262,7 @@ class KernelNN(torch.nn.Module):
         self.x_position_dim = x_position_dim
 
         self.lstm = nn.LSTM(x_position_dim, x_position_dim)
-        self.lstm_fc = torch.nn.Linear(x_position_dim, x_position_dim)
+        self.lstm_fc = torch.nn.Linear(x_position_dim, x_position_dim+embedding_dim)
 
         # self.emb = nn.Embedding(num_embeddings, embedding_dim)
 
