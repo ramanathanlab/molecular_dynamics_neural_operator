4e9435bb116fde056f3e7daf32f37b2092be393b
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2021-11-07 15:29:16 -0700

    fixing make prop movie

--- graph_kernel.py
+++ graph_kernel.py
@@ -585,7 +585,7 @@ def main():
         avg_valid_loss, avg_valid_mse = validate(model, valid_loader, loss_fn, device)
         video = None
         if args.generate_movie and (epoch % args.plot_per_epochs == 0):
-            make_propagation_movie(model, valid_dataset, device, args.num_movie_frames, starting_points=starting_points, epoch=epoch)
+            make_propagation_movie(model, valid_dataset, device, num_steps=args.num_movie_frames, starting_points=starting_points, epoch=epoch)
             video = wandb.Video(args.run_path / 'epoch{}_gno_movie.mp4'.format(epoch), fps=2, format="mp4")
         if args.plot_latent and (epoch % args.plot_per_epochs == 0):
             with torch.no_grad():
