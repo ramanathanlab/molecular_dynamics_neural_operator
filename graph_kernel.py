6142253b2714b52a870bbb80f3cd71d585b9daaf
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2021-12-07 11:15:43 -0700

    Update graph_kernel.py

--- graph_kernel.py
+++ graph_kernel.py
@@ -522,7 +522,7 @@ def main():
     print("Split training and validation sets")
 
     # Setup device
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    device = torch.device("xpu")
 
     # Setup model, optimizer, loss function and scheduler
     model = DataParallel(KernelNN(
